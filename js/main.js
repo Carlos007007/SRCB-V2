d3d107b1088c2269fa7fc79f380b805f04e48e47
carlosedu007007@gmail.com <7BlackAdobeCC2017>
2018-11-04 09:59:34 -0600

    Update

---
+++ js/main.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+    $('.tooltips-general').tooltip('hide');
+    $('.mobile-menu-button').on('click', function(){
+        var mobileMenu=$('.navbar-lateral');	
+        if(mobileMenu.css('display')=='none'){
+            mobileMenu.fadeIn(300);
+        }else{
+            mobileMenu.fadeOut(300);
+        }
+    });
+    $('.desktop-menu-button').on('click', function(e){
+        e.preventDefault();
+        var NavLateral=$('.navbar-lateral'); 
+        var ContentPage=$('.content-page-container');   
+        if(NavLateral.hasClass('desktopMenu')){
+            NavLateral.removeClass('desktopMenu');
+            ContentPage.removeClass('desktopMenu');
+        }else{
+            NavLateral.addClass('desktopMenu');
+            ContentPage.addClass('desktopMenu');
+        }
+    });
+    $('.dropdown-menu-button').on('click', function(e){
+        e.preventDefault();
+        var icon=$(this).children('.icon-sub-menu');
+        if(icon.hasClass('zmdi-chevron-down')){
+            icon.removeClass('zmdi-chevron-down').addClass('zmdi-chevron-up');
+            $(this).addClass('dropdown-menu-button-active');
+        }else{
+            icon.removeClass('zmdi-chevron-up').addClass('zmdi-chevron-down');
+            $(this).removeClass('dropdown-menu-button-active');
+        }
+        
+        var dropMenu=$(this).next('ul');
+        dropMenu.slideToggle('slow');
+    });
+    $('.exit-system-button').on('click', function(e){
+        e.preventDefault();
+        var LinkExitSystem=$(this).attr("data-href");
+        swal({
+            title: "¿Estás seguro?",
+            text: "Quieres salir del sistema y cerrar la sesión actual",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#5cb85c",
+            confirmButtonText: "Si, salir",
+            cancelButtonText: "No, cancelar",
+            animation: "slide-from-top",
+            closeOnConfirm: false 
+        },function(){
+            window.location=LinkExitSystem; 
+        });  
+    });
+    $('.search-book-button').click(function(e){
+        e.preventDefault();
+        var LinkSearchBook=$(this).attr("data-href");
+        swal({
+           title: "¿Qué libro estás buscando?",
+           text: "Por favor escribe el nombre del libro",
+           type: "input",   
+           showCancelButton: true,
+           closeOnConfirm: false,
+           animation: "slide-from-top",
+           cancelButtonText: "Cancelar",
+           confirmButtonText: "Buscar",
+           confirmButtonColor: "#3598D9",
+           inputPlaceholder: "Escribe aquí el nombre de libro" }, 
+      function(inputValue){
+           if (inputValue === false) return false;  
+
+           if (inputValue === "") {
+               swal.showInputError("Debes escribir el nombre del libro");     
+               return false;   
+           } 
+            window.location=LinkSearchBook+"?bookName="+inputValue;
+       });
+    });
+    $('.btn-help').on('click', function(){
+        $('#ModalHelp').modal({
+            show: true,
+            backdrop: "static"
+        });
+    });
+});
+(function($){
+    $(window).load(function(){
+        $(".nav-lateral-scroll").mCustomScrollbar({
+            theme:"light-thin",
+            scrollbarPosition: "inside",
+            autoHideScrollbar: true,
+            scrollButtons:{ enable: true }
+        });
+        $(".custom-scroll-containers").mCustomScrollbar({
+            theme:"dark-thin",
+            scrollbarPosition: "inside",
+            autoHideScrollbar: true,
+            scrollButtons:{ enable: true }
+        });
+    });
+})(jQuery);
